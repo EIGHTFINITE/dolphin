5f372d6b1df2a6c87cf54e4f6eeb5c0e12383739
JosJuice <josjuice@gmail.com>
2026-06-19 11:37:04 +0200

    VideoCommon: Reduce padding in PresentInfo
    This makes the struct 8 bytes smaller.

--- Source/Core/VideoCommon/Present.cpp
+++ Source/Core/VideoCommon/Present.cpp
@@ -234,9 +234,9 @@ void Presenter::ImmediateSwap(u32 xfb_addr, u32 fb_width, u32 fb_stride, u32 fb_
   PresentInfo present_info{
       .frame_count = m_frame_count++,
       .present_count = m_present_count++,
-      .reason = PresentInfo::PresentReason::Immediate,
       .emulated_timestamp = ticks,
       .intended_present_time = m_next_swap_estimated_time,
+      .reason = PresentInfo::PresentReason::Immediate,
   };
 
   auto& video_events = GetVideoEvents();
